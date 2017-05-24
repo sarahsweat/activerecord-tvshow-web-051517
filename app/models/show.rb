7bd81bf49586260f0d1d7797b9e0f93b284a433e
sarahsweat <sarahsweat@gmail.com>
2017-05-24 18:00:42 -0400

    Done.

---
+++ app/models/show.rb
@@ -0,0 +1,33 @@
+class Show < ActiveRecord::Base
+
+  def self.highest_rating
+    Show.order(rating: :desc).first.rating
+
+  end
+
+  def self.most_popular_show
+    Show.order(rating: :desc).first
+  end
+
+  def self.lowest_rating
+    Show.order(rating: :desc).last.rating
+  end
+
+  def self.least_popular_show
+    Show.order(rating: :desc).last
+  end
+
+  def self.ratings_sum
+    Show.sum(:rating)
+  end
+
+  def self.popular_shows
+    Show.where("rating > 5")
+  end
+
+  def self.shows_by_alphabetical_order
+    Show.order(:name)
+  end
+
+
+end
